b68b49a5b46d3e6f1ded1ae79a208fa5b6f06a5c
D TV <soldierofgod35@gmail.com>
2023-05-17 00:04:11 -0700

    second commit

--- pipe.js
+++ pipe.js
@@ -1,41 +1,45 @@
-function Pipe() {
-  this.spacing = 175;
-  this.top = random(height / 6, (3 / 4) * height);
-  this.bottom = height - (this.top + this.spacing);
-  this.x = width;
-  this.w = 80;
-  this.speed = 2;
-  this.highlight = false;
-
-  this.show = function () {
+function Pipe(gameover) {
+    this.spacing = 175;
+    this.top = random(height / 6, (3 / 4) * height);
+    this.bottom = height - (this.top + this.spacing);
+    this.x = width;
+    this.w = 80;
+    this.speed = 2;
+    this.scored = false;
+    this.gameover = gameover;
+  
+    this.show = function () {
       fill(255);
       if (this.highlight) {
-          fill(255, 0, 0);
+        fill(255, 0, 0);
       }
       rect(this.x, 0, this.w, this.top);
       rect(this.x, height - this.bottom, this.w, this.bottom);
-  };
-
-  this.update = function () {
+    };
+  
+    this.update = function () {
       this.x -= this.speed;
-  };
-
-  this.offscreen = function () {
+    };
+  
+    this.offscreen = function () {
       if (this.x < -this.w) {
-          return true;
+        return true;
       }
       return false;
-  };
-
-  this.hits = function (bird) {
+    };
+  
+    this.hits = function (bird) {
       if (
-          (bird.y < this.top || bird.y > height - this.bottom) &&
-          bird.x > this.x &&
-          bird.x < this.x + this.w
+        (bird.y < this.top || bird.y > height - this.bottom) &&
+        bird.x > this.x &&
+        bird.x < this.x + this.w
       ) {
+        if (!this.scored) {
+          this.scored = true;
           return true;
+        }
       }
       return false;
-  };
-}
-        
+    };
+  }
+  
--- sketch.js
+++ sketch.js
@@ -1,58 +1,79 @@
 var bird;
 var pipes = [];
 var gameover = false;
+var fails = 0;
+var timer = 0;
 
 function setup() {
-    createCanvas(400, 600);
-    bird = new Bird();
-    pipes.push(new Pipe());
+  createCanvas(400, 600);
+  bird = new Bird();
+  pipes.push(new Pipe(gameover));
 }
 
 function draw() {
-    background(0, 191, 255); // Sea blue background
+  background(0, 191, 255); // Sea blue background
 
-    if (!gameover) {
-        for (var i = pipes.length - 1; i >= 0; i--) {
-            pipes[i].show();
-            pipes[i].update();
+  if (!gameover) {
+    for (var i = pipes.length - 1; i >= 0; i--) {
+      pipes[i].show();
+      pipes[i].update();
 
-            if (pipes[i].hits(bird)) {
-                gameover = true;
-                console.log("GAME OVER");
-            }
-
-            if (pipes[i].offscreen()) {
-                pipes.splice(i, 1);
-            }
+      if (bird.hits(pipes[i])) {
+        if (!pipes[i].scored) {
+          fails++;
+          pipes[i].scored = true;
         }
+      }
 
-        bird.update();
-        bird.show();
-
-        if (frameCount % 100 === 0) {
-            pipes.push(new Pipe());
-        }
-    } else {
-        // Game over state
-        textAlign(CENTER);
-        textSize(32);
-        fill(255);
-        text("GAME OVER", width / 2, height / 2);
+      if (pipes[i].offscreen()) {
+        pipes.splice(i, 1);
+      }
     }
+
+    bird.update();
+    bird.show();
+
+    if (frameCount % 100 === 0) {
+      pipes.push(new Pipe(gameover));
+    }
+  } else {
+    // Game over state
+    textAlign(CENTER);
+    textSize(32);
+    fill(255);
+    text("GAME OVER", width / 2, height / 2);
+  }
+
+  // Display fails
+  textSize(24);
+  fill(255);
+  text("Fails: " + fails, 10, 30);
+
+  // Display timer
+  textSize(24);
+  fill(255);
+  text("Timer: " + Math.floor(timer / 60) + "s", width - 150, 30);
+
+  // Update timer
+  if (!gameover) {
+    timer++;
+  }
 }
 
 function keyPressed() {
-    if (key === " " && !gameover) {
-        bird.up();
-    }
+  if (key === " ") {
+    bird.up();
+  }
 }
 
 function mousePressed() {
-    if (gameover) {
-        // Reset the game
-        gameover = false;
-        pipes = [];
-        bird = new Bird();
-        pipes.push(new Pipe());
-    }
+  if (gameover) {
+    // Reset the game
+    gameover = false;
+    pipes = [];
+    bird = new Bird();
+    pipes.push(new Pipe(gameover));
+    fails = 0;
+    timer = 0;
+  }
 }
